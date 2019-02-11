86e943f9e050702140ea9934401007c3d415b7cb
breathfisheva <hongliu_goodluck@sina.com>
2019-02-11 14:46:53 +0800

    version5: update the handlers.py“

--- handlers.py
+++ handlers.py
@@ -1,3 +1,8 @@
+'''
+之前我们的handler函数要传给add_route，add_route要求handler是coroutine，
+可是我们现在用RequestHandler封装了handler函数，在RequestHandler里我们await了handler方法，所以handler函数可以不需要是coroutine
+'''
+
 from aiohttp import web
 from coroweb import *
 import asyncio
@@ -5,12 +10,10 @@ import asyncio
 
 #3.给每个handler函数增加__method__属性和__route__属性。
 @get('/')
-async def index(request):
-    await asyncio.sleep(0.5)
+def index(request):
     return web.Response(body=b'<h1>Index</h1>', content_type='text/html')
 
 @get('/hello/{name}')
-async def hello(request, *, name):
-    await asyncio.sleep(0.5)
+def hello(request, *, name):
     text = '<h1>hello, %s!</h1>' % request.match_info['name']
     return web.Response(body=text.encode('utf-8'), content_type='text/html')
